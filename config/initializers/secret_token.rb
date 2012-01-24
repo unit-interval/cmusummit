2f5bce9eaba618d63a8821eea876c06bc1cc0bf0
Tao <tao@huangtao.me>
2012-01-24 17:24:39 +0800

    rails new, and bundle install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cmusummit::Application.config.secret_token = 'b4ac021307d89a2f906ff5aecc0c35fc85ad73ff17b086fff446590de36e5b827ca950de97c044218979b2c195361692dfe0bd54650305b9ea598701027ae453'
