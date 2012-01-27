903159cb53e4ed73e0d4b4b7ec0ced5e2c1ad8fd
Zilin Jiang <libragold@Zilins-MacBook-Air.local>
2012-01-27 00:58:46 -0500

    Apply New Design to Homepage and Navigation Bar

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -17,4 +17,31 @@ $(function() {
     $("#info #" + a).addClass("active");
     return false;
   });
+	$("#top h3").delay(2000).animate({'margin-top': 0}, 500);
+	$(".arrow-nav a.up").click(function(){
+		var f=$(this);
+		var c=f.parent().siblings('.content');
+		var d=c.scrollTop()
+		var e=f.siblings('a.down');
+		if (d!=0) c.animate({scrollTop: d-200}, 500, function(){
+			if (c.scrollTop()==0) {
+				f.addClass('up-inactive');
+				e.removeClass('down-inactive');
+			}
+		});
+		return false;
+	})
+	$(".arrow-nav a.down").click(function(){
+		var e=$(this);
+		var c=e.parent().siblings('.content');
+		var d=c.scrollTop();
+		var f=e.siblings('a.up');
+		if (d<c.get(0).scrollHeight-c.height()) c.animate({scrollTop: d+200}, 500, function(){
+			if (c.scrollTop()==c.get(0).scrollHeight-c.height()) {
+				e.addClass('down-inactive');
+				f.removeClass('up-inactive');
+			}			
+		});
+		return false;
+	})
 });
